84d65bb0a805a38b034852dfb5315e38320101ea
Saulo <saulo972shp@hotmail.com>
2022-11-02 16:33:47 -0300

    Initial commit

---
+++ foo/src/test/java/br/com/foobar/foo/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.foobar.foo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
